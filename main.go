1ccd70c9667933786d7000c352c2cac370a7ec1c
zangarmarsh <alessandro@webshapes.it>
2022-09-10 09:28:19 +0200

    core/ui: verbosity can be enabled by the cli argument -v (still, the project needs some refactoring and clean up in order to fully support a proper logging)

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"flag"
 	"github.com/zangarmarsh/inplainsight/ui"
 	"github.com/zangarmarsh/inplainsight/ui/pages"
 	_ "github.com/zangarmarsh/inplainsight/ui/pages/list"
 	_ "github.com/zangarmarsh/inplainsight/ui/pages/login"
+	_ "github.com/zangarmarsh/inplainsight/ui/pages/newsecret"
 	_ "github.com/zangarmarsh/inplainsight/ui/pages/register"
+	"io"
 	"log"
 )
 
+var verbosity bool
+
 func main() {
-	ui.Bootstrap( )
+	{
+		// handle flags in here
+		setLoggingLevel()
+	}
+
+	ui.Bootstrap()
 	pages.Init()
 
 	err := ui.InPlainSight.App.Run()
 	if err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func setLoggingLevel() {
+	flag.BoolVar(&verbosity, "v", false, "allow verbosity")
+	flag.Parse()
+
+	if !verbosity {
+		log.SetOutput(io.Discard)
 	}
 }
 
