ebc6dfdcdc0014c2cea4dfaa9a3a60a7a9b2a4ad
zangarmarsh <alessandro@webshapes.it>
2022-09-10 09:46:48 +0200

    ui/modal: improved the way the success/error modal are handled

--- ui/widgets/modal.go
+++ ui/widgets/modal.go
@@ -6,30 +6,55 @@ import (
 	"github.com/zangarmarsh/inplainsight/ui"
 )
 
+var modalSuccess *tview.Modal
 func ModalSuccess(text string) tview.Primitive {
-	modal := tview.NewModal()
+	var newlyCreated bool
+	pageName := "modal-success"
 
-	modal.SetText(text).
+	if modalSuccess == nil {
+		modalSuccess = tview.NewModal()
+		newlyCreated = true
+	}
+
+	modalSuccess.SetText(text).
 		AddButtons([]string{"OK"}).
 		SetFocus(0).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-
+			ui.InPlainSight.Pages.HidePage(pageName)
 		})
 
-	return modal
+	if newlyCreated {
+		ui.InPlainSight.Pages.AddAndSwitchToPage( pageName, modalSuccess, true )
+	} else {
+		ui.InPlainSight.Pages.SwitchToPage(pageName)
+	}
+
+	return modalSuccess
 }
 
+var modalError *tview.Modal
 func ModalError(text string) tview.Primitive {
-	modal := tview.NewModal()
+	var newlyCreated bool
+	pageName := "modal-error"
 
-	modal.SetText( "Wrong password" ).
+	if modalError == nil {
+		modalError = tview.NewModal()
+		newlyCreated = true
+	}
+
+	modalError.SetText( text ).
 		AddButtons([]string{"OK"}).
 		SetFocus(0).
 		SetBackgroundColor(tcell.ColorRed).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			ui.InPlainSight.Pages.HidePage("modal-error")
+			ui.InPlainSight.Pages.HidePage(pageName)
 		})
 
-	return modal
-}
+	if newlyCreated {
+		ui.InPlainSight.Pages.AddAndSwitchToPage( pageName, modalError, true )
+	} else {
+		ui.InPlainSight.Pages.SwitchToPage(pageName)
+	}
 
+	return modalError
+}
