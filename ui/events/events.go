48292a3268d00060c8dfafad69a043b70bf8cfeb
zangarmarsh <alessandro@webshapes.it>
2022-09-10 09:26:58 +0200

    core/ui: implement an event listener queue feature

---
+++ ui/events/events.go
@@ -0,0 +1,40 @@
+package events
+
+import (
+	"time"
+)
+
+type EventType uint8
+
+const (
+	DiscoveredNewSecret EventType = iota
+	AddedNewSecret
+	UpdatedSecret
+)
+
+type Event struct {
+	CreatedAt  time.Time
+	EventType EventType
+	Data      map[string]interface{}
+}
+
+type EventeableStruct struct {
+	listenersQueue  map[EventType][]func(event Event)
+	triggeredEvents []Event
+}
+
+func (e *EventeableStruct) AddEventsListener(eventsType []EventType, callback func(event Event)) {
+	if e.listenersQueue == nil {
+		e.listenersQueue = make(map[EventType][]func(event Event))
+	}
+
+	for _, eventType := range eventsType {
+		e.listenersQueue[eventType] = append(e.listenersQueue[eventType], callback)
+	}
+}
+
+func (e *EventeableStruct) Trigger(event Event) {
+	for _, callback := range e.listenersQueue[event.EventType] {
+		callback(event)
+	}
+}
