d0b14a5e2f3f1305d8b6f96af4324063105e7c74
zangarmarsh <alessandro@webshapes.it>
2022-09-10 09:43:01 +0200

    core: add Secret struct in order to support a more complex data structure than the basic one provided from inplainsight which actually just allows to save a string

--- ui/ui.go
+++ ui/ui.go
@@ -8,6 +8,31 @@ import (
 	"strings"
 )
 
+type Secret struct {
+	Title        string
+	Description  string
+	Secret       string
+	FilePath     string
+}
+
+const secretSeparator = "\x02"
+
+func (s *Secret ) Serialize() string  {
+	return s.Title + secretSeparator + s.Description + secretSeparator + s.Secret
+}
+
+func (s *Secret) Unserialize(serialized string) {
+	fields := strings.Split(serialized, secretSeparator)
+
+	if len(fields) == 3 {
+		s.Title = fields[0]
+		s.Description = fields[1]
+		s.Secret = fields[2]
+	} else {
+		log.Printf("Cannot unserialize serialized secret %#v\n", serialized)
+	}
+}
+
 type InPlainSightClient struct {
 	events.EventeableStruct
 
