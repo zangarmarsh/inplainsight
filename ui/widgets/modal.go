be27443f361e25c0168249073a610968b5749033
zangarmarsh <alessandro@webshapes.it>
2024-07-11 00:58:04 +0200

    ui: throwaway modals logic

--- ui/widgets/modal.go
+++ ui/widgets/modal.go
@@ -6,55 +6,37 @@ import (
 	"github.com/zangarmarsh/inplainsight/ui"
 )
 
-var modalSuccess *tview.Modal
 func ModalSuccess(text string) tview.Primitive {
-	var newlyCreated bool
 	pageName := "modal-success"
-
-	if modalSuccess == nil {
-		modalSuccess = tview.NewModal()
-		newlyCreated = true
-	}
+	modalSuccess := tview.NewModal()
 
 	modalSuccess.SetText(text).
 		AddButtons([]string{"OK"}).
 		SetFocus(0).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			ui.InPlainSight.Pages.HidePage(pageName)
+			ui.InPlainSight.Pages.RemovePage(pageName)
 		})
 
-	if newlyCreated {
-		ui.InPlainSight.Pages.AddAndSwitchToPage( pageName, modalSuccess, true )
-	} else {
-		ui.InPlainSight.Pages.SwitchToPage(pageName)
-	}
+	ui.InPlainSight.Pages.AddAndSwitchToPage(pageName, modalSuccess, true)
 
 	return modalSuccess
 }
 
-var modalError *tview.Modal
 func ModalError(text string) tview.Primitive {
-	var newlyCreated bool
 	pageName := "modal-error"
 
-	if modalError == nil {
-		modalError = tview.NewModal()
-		newlyCreated = true
-	}
+	modalError := tview.NewModal()
+	modalError.AddButtons([]string{"OK"})
 
-	modalError.SetText( text ).
-		AddButtons([]string{"OK"}).
+	modalError.
+		SetText(text).
 		SetFocus(0).
 		SetBackgroundColor(tcell.ColorRed).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			ui.InPlainSight.Pages.HidePage(pageName)
+			ui.InPlainSight.Pages.RemovePage(pageName)
 		})
 
-	if newlyCreated {
-		ui.InPlainSight.Pages.AddAndSwitchToPage( pageName, modalError, true )
-	} else {
-		ui.InPlainSight.Pages.SwitchToPage(pageName)
-	}
+	ui.InPlainSight.Pages.AddAndSwitchToPage(pageName, modalError, true)
 
 	return modalError
 }
