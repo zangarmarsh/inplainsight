492cf6fc9281990f9970d6510e1e3434a6fe4f47
zangarmarsh <alessandro@webshapes.it>
2024-08-07 13:55:13 +0200

    feature: lockscreendetector for macOS

--- core/utility/lockscreendetector/lockscreendetector_darwin.go
+++ core/utility/lockscreendetector/lockscreendetector_darwin.go
@@ -1,5 +1,9 @@
 package lockscreendetector
 
 func isScreenLocked() bool {
-	return false
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	output, _ := exec.CommandContext(ctx, "ioreg", "ioreg", "-n", "Root", "-d1").Output()
+	return strings.Contains(string(output), "CGSSessionScreenIsLocked")
 }
